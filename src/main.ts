fd34bcfe4ec6513c8a88b9453b97d6c058505978
Victor Medina <vmedina.qcode.ext@luegopago.com>
2022-08-22 10:47:09 -0500

    config custom element

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+/*import { createApp } from "vue";
+import App from "./app/App.vue";
 
 createApp(App).mount("#app");
+*/
+
+import { defineCustomElement } from "vue";
+import App from "./app/App.ce.vue";
+
+const app = defineCustomElement(App);
+
+customElements.define("widget-vue", app);
